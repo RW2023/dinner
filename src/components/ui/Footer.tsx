ab9d5af2d1052f66f845688f5b3774c8eece52ea
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-31 18:55:29 -0500

    Update GitHub link in Footer component

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -13,8 +13,8 @@ const Footer = () => {
         <div className="grid grid-flow-col gap-4 text-4xl">
           {/* GitHub */}
           <a
-            href="https://github.com/RW2023/kids"
-            target="https://github.com/RW2023/kids"
+            href="https://github.com/RW2023/dinner"
+            target="https://github.com/RW2023/dinner"
             rel="noopener noreferrer"
             aria-label="GitHub"
           >
