f58aa85aa46d82573804b53a2b26618635fe9fca
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-31 18:48:12 -0500

    remove background and text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 //src/app/page.tsx
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen text-5xl font-bold font-sans bg-black text-white">
+    <div className="flex flex-col justify-center items-center text-5xl font-bold font-sans min-h-screen">
       <h1>Home</h1>
     </div>
   )
