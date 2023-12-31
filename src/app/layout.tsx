3eea971b25fcc5128625787036a080abb137a394
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-31 18:47:29 -0500

    Add Navbar and Footer components to layout.tsx. also qdd font awesome link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Navbar from '@/components/ui/Navbar';
+import Footer from '@/components/ui/Footer';
 
 export const metadata: Metadata = {
   title: 'Eats APP',
@@ -13,7 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+      </head>
+      <body>
+          <Navbar />
+        {children}
+      </body>
+      <Footer />
     </html>
   );
 }
